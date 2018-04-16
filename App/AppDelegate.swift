677ae7ed8653e1ff6c16e9eb03ea398b6bf34136
TimChen <timchen@kkstream.com.tw>
2018-04-16 11:23:55 +0800

    To reproduce _TtC14view_framework8PiPiView error message

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let _ = PiPiView()
+        // uncomment below code to show gradient effect
+        //let _ = PiPiView()
         return true
     }
 
