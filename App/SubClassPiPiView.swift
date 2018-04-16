9676dbe60d4e7158b9c3e6ad1e6a4736827b1831
TimChen <timchen@kkstream.com.tw>
2018-04-16 12:16:20 +0800

    SubClassPiPiView can used in Main.storyboard

---
+++ App/SubClassPiPiView.swift
@@ -0,0 +1,21 @@
+//
+//  SubClassPiPiView.swift
+//  App
+//
+//  Created by Tim Chen on 2018/4/16.
+//  Copyright © 2018 PiPi. All rights reserved.
+//
+
+import UIKit
+import view_framework
+class SubClassPiPiView: PiPiView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
